073c87e034f2d6ad627c0ad4fa19cf3d192b0a28
atreya18 <dasatreya@gmail.com>
2018-10-18 18:34:16 +0530

    made changes to app.use

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const socketServer = require ('./socket-connection/index.js');
 const port = process.env.PORT || 3000;
 const collaboration = require ('./api/collaboration')
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+ });
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use("/api/v1", collaboration)
 
