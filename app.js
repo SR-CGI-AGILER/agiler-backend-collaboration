1b9e2808ffd72329dc68b7ac775e8368ea6e7844
Ankit-Mund <ankit7mund@gmail.com>
2018-10-18 18:24:57 +0530

    CORS error resolved

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const port = process.env.PORT || 3000;
 const collaboration = require('./api/collaboration')
 const logger = require('morgan')
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
+
+
 app.use(logger('dev'))
 
 app.use(bodyParser())
